645e8450d554c98c9b8f85f010d3104a03166829
artyomsoft <artyomsoft@gmail.com>
2025-12-08 16:55:58 +0200

    Implemented f2fs support

---
+++ README.md
@@ -0,0 +1,12 @@
+# Destiption
+Linux system with f2fs root file system
+
+# Usage
+
+```
+$ mkdir work && cd work 
+$ git clone https://github.com/artyomsoft/f2fs-pc-buildroot.git
+$ cd f2fs-pc-buildroot
+$ ./build.sh
+```
+
---
+++ build.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -e
+
+TREE_DIR=$(pwd)
+
+wget https://buildroot.org/downloads/buildroot-2025.08.2.tar.gz
+
+mkdir ../buildroot && tar xvf buildroot-2025.08.2.tar.gz --strip-components=1 -C ../buildroot
+
+mkdir ../buildroot/board/pc/patches
+
+cd ../buildroot
+
+make BR2_EXTERNAL="$TREE_DIR" f2fs_pc_bios_defconfig
+
+make
+
+qemu-system-x86_64 -M pc -drive file=output/images/disk.img,if=virtio,format=raw,cache=writeback
+
