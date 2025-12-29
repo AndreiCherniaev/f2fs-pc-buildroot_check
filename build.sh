409ad9acb6a6744eb51b50c8258bd263092d057d
Andrei Cherniaev <27889022+AndreiCherniaev@users.noreply.github.com>
2025-12-29 23:02:35 +0900

    add folder with space support
    even home folder sometimes can contains space

--- build.sh
+++ build.sh
@@ -2,19 +2,18 @@
 
 set -e
 
-TREE_DIR=$(pwd)
+TREE_DIR="$(pwd)"
 
 wget https://buildroot.org/downloads/buildroot-2025.08.2.tar.gz
 
-mkdir ../buildroot && tar xvf buildroot-2025.08.2.tar.gz --strip-components=1 -C ../buildroot
+mkdir "../buildroot" && tar xvf buildroot-2025.08.2.tar.gz --strip-components=1 -C "../buildroot"
 
 mkdir -p "../buildroot/board/pc/patches"
 
-cd ../buildroot
+cd "../buildroot"
 
 make BR2_EXTERNAL="$TREE_DIR" f2fs_pc_bios_defconfig
 
 make
 
 qemu-system-x86_64 -M pc -drive file=output/images/disk.img,if=virtio,format=raw,cache=writeback
-
